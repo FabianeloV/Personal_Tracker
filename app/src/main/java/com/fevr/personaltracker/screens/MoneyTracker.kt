c9679d68652ea3a5a54a8da50ef93aeeea15a4b4
Fabianelo <fevr.developer@gmail.com>
2024-06-07 06:04:40 -0500

    7 mayo - rmv unused import

--- app/src/main/java/com/fevr/personaltracker/screens/MoneyTracker.kt
+++ app/src/main/java/com/fevr/personaltracker/screens/MoneyTracker.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Add
 import androidx.compose.material.icons.outlined.ArrowDropDown
-import androidx.compose.material.icons.outlined.CheckCircle
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardColors
